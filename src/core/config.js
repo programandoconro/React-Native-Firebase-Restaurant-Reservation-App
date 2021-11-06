d6a1634b97e73d3eaf04b50ab8d561c29ca75860
Ro <50117686+programandoconro@users.noreply.github.com>
2021-11-06 12:03:53 +0900

    Update config

--- src/core/config.js
+++ src/core/config.js
@@ -1,14 +1,14 @@
 // Replace with your own firebase config!
 export const FIREBASE_CONFIG = {
 
-    apiKey: "AIzaSyAewV4oxgwbq0r8ejPWif2_EnOw7LecebM",
-    authDomain: "cactus-pro.firebaseapp.com",
-    databaseURL: "https://cactus-pro.firebaseio.com",
-    projectId: "cactus-pro",
-    storageBucket: "cactus-pro.appspot.com",
-    messagingSenderId: "16520205636",
-    appId: "1:16520205636:web:f1407e1c4e27814b95b311",
-    measurementId: "G-R44C467LQR"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 
 
 };
